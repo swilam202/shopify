010b7cee9fc27bb91571c92692c131e46cd8b860
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2023-10-25 01:07:05 +0300

    finish auth page ui

--- lib/auth feature/presenetaion/pages/auth page.dart
+++ lib/auth feature/presenetaion/pages/auth page.dart
@@ -6,6 +6,7 @@ import 'package:hhhhhhhh/auth%20feature/presenetaion/controller/auth%20state.dar
 import 'package:hhhhhhhh/auth%20feature/presenetaion/widgets/auth%20row%20items.dart';
 import 'package:hhhhhhhh/auth%20feature/presenetaion/widgets/auth%20text%20form%20field%20section.dart';
 import 'package:hhhhhhhh/auth%20feature/presenetaion/widgets/positions%20enum.dart';
+import 'package:hhhhhhhh/core/styles/style%20colors.dart';
 import 'package:hhhhhhhh/core/styles/text%20styles.dart';
 import 'package:hhhhhhhh/core/widgets/custom%20button.dart';
 
@@ -21,30 +22,50 @@ class AuthPage extends StatelessWidget {
         child: BlocBuilder<AuthCubit, AuthState>(
           builder: (context, state) {
             return Padding(
-          padding: EdgeInsets.only(top: 50),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              padding: const EdgeInsets.only(top: 50),
+              child: Column(
                 children: [
-                  GestureDetector(
-                    onTap: ()=>BlocProvider.of<AuthCubit>(context).switchAuthMode(isLogin: true),
-                    child: LoginRowItem(isLogin: (state is LoginState)? true:false,)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      GestureDetector(
+                          onTap: () => BlocProvider.of<AuthCubit>(context)
+                              .switchAuthMode(isLogin: true),
+                          child: LoginRowItem(
+                            isLogin: (state is LoginState) ? true : false,
+                          )),
+                      GestureDetector(
+                        onTap: () => BlocProvider.of<AuthCubit>(context)
+                            .switchAuthMode(isLogin: false),
+                        child: SignupRowItem(
+                          isLogin: (state is SignupState) ? true : false,
+                        ),
+                      ),
+                    ],
                   ),
-                  GestureDetector(
-                    onTap: ()=>BlocProvider.of<AuthCubit>(context).switchAuthMode(isLogin: false),
-                    child: SignupRowItem(isLogin: (state is SignupState)? true:false,),),
+                  const SizedBox(height: 50),
+                  (state is LoginState)
+                      ? const AuthLogInTextFieldSection()
+                      : const AuthSignUpTextFieldSection(),
+                  const SizedBox(height: 50),
+                  CustomButton(
+                    isLogin: (state is LoginState) ? true : false,
+                  ),
+                  const SizedBox(height: 30),
+                  const Text(
+                    'By creating an account, you agree to our',
+                    textAlign: TextAlign.center,
+                    style: TextStyles.style12Light,
+                  ),
+                  Text(
+                    'Terms of Service and Privacy Policy',
+                    textAlign: TextAlign.center,
+                    style: TextStyles.style12Light
+                        .copyWith(color: StyleColor.orangeColor),
+                  ),
                 ],
               ),
-                ///AuthLogInTextFieldSection(),
-                  SizedBox(height: 50),
-                  (state is LoginState)? const AuthLogInTextFieldSection():const AuthSignUpTextFieldSection(),
-                  SizedBox(height: 50),
-                  CustomButton(isLogin: (state is LoginState)? true:false,),
-                
-            ],
-          ),
-        );
+            );
           },
         ),
       ),
