7d4437abb6c7435a8350c78aed8675c2730c2d2f
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2023-10-26 20:33:51 +0300

    more work on checkout page

--- lib/features/cart feature/presentation/widgets/checkout payment section.dart
+++ lib/features/cart feature/presentation/widgets/checkout payment section.dart
@@ -8,12 +8,20 @@ class CheckoutPaymentSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text('PAYMENT METHOD', style: TextStyles.style12Medium),
+        Text(
+          'PAYMENT METHOD',
+          style: TextStyles.style12Medium.copyWith(
+            color: const Color(0xff515C6F).withOpacity(0.5),
+          ),
+        ),
         ListTile(
           leading: const Icon(Icons.credit_card),
-          title: const Text('Master Card ending 2800',
-              style: TextStyles.style15Medium),
+          title: const Text(
+            'Master Card ending 2800',
+            style: TextStyles.style15Medium,
+          ),
           trailing: IconButton(
             onPressed: () {},
             icon: const Icon(Icons.forward),
