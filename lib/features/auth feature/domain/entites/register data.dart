a0c5fd05377eca65430cfc1739a85cc3b265ffe1
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2023-11-09 16:20:11 +0200

    register and data entity

---
+++ lib/features/auth feature/domain/entites/register data.dart
@@ -0,0 +1,17 @@
+class RegisterData {
+  final int id;
+  final String name;
+  final String phone;
+  final String email;
+  final String token;
+  final String image;
+
+  RegisterData({
+    required this.id,
+    required this.name,
+    required this.phone,
+    required this.email,
+    required this.token,
+    required this.image,
+  });
+}
---
+++ lib/features/auth feature/domain/entites/register.dart
@@ -0,0 +1,13 @@
+import 'package:hhhhhhhh/features/auth%20feature/domain/entites/register%20data.dart';
+
+class Register {
+  final bool status;
+  final String message;
+  final RegisterData data;
+
+  Register({
+    required this.status,
+    required this.message,
+    required this.data,
+  });
+}
